ee418e94bf84f7fd1b09f0869551404f15d1f9e3
PeterJeng <peterjeng1@gmail.com>
2019-04-30 00:51:29 -0400

    Added example code for brian2 to generate 1000 neurons

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os
 from PIL import Image
-
+from brian2 import *
+from matplotlib import pyplot
 
 class LifNeuron:
     def __init__(self, weights):
@@ -70,39 +71,59 @@ class NeuralNetwork:
     def __init__(self):
         self.layer1 = []
 
+def brian_example():
+    eqs = '''
+    dv/dt  = (ge+gi-(v+49*mV))/(20*ms) : volt
+    dge/dt = -ge/(5*ms)                : volt
+    dgi/dt = -gi/(10*ms)               : volt
+    '''
+    P = NeuronGroup(4000, eqs, threshold='v>-50*mV', reset='v=-60*mV')
+    P.v = -60 * mV
+    Pe = P[:3200]
+    Pi = P[3200:]
+    Ce = Synapses(Pe, P, on_pre='ge+=1.62*mV')
+    Ce.connect(p=0.02)
+    Ci = Synapses(Pi, P, on_pre='gi-=9*mV')
+    Ci.connect(p=0.02)
+    M = SpikeMonitor(P)
+    run(1 * second)
+    plot(M.t / ms, M.i, '.')
+    show()
 
 if __name__ == '__main__':
-    image_width = 500
-    image_height = 333
-    # Set up neural network
-    network = NeuralNetwork()
-    for row in range(0, image_height):
-        network.layer1.append([])
-        for col in range(0, image_width):
-            network.layer1[row].append(LifNeuron(0))
+    # image_width = 500
+    # image_height = 333
+    # # Set up neural network
+    # network = NeuralNetwork()
+    # for row in range(0, image_height):
+    #     network.layer1.append([])
+    #     for col in range(0, image_width):
+    #         network.layer1[row].append(LifNeuron(0))
+    #
+    # # Traverse images in image_resized folder
+    # rootDir = 'images/'
+    # for dirName, subdirList, fileList in os.walk(rootDir):
+    #     for fileN in fileList:
+    #         # Get dog type from folder name
+    #         dirNameSplit = dirName.split('-')
+    #         dogName = dirNameSplit[1]
+    #
+    #         # Convert image into black and white. Rescale to 500x333 image
+    #         newDirName = dirName.replace('\\', '/')
+    #         img = Image.open(newDirName + '/' + fileN)
+    #         img = img.resize((image_width, image_height), Image.ANTIALIAS)
+    #         img = img.convert('L')
+    #         WIDTH, HEIGHT = img.size
+    #         data = list(img.getdata())
+    #         data = [data[offset:offset + WIDTH] for offset in range(0, WIDTH * HEIGHT, WIDTH)]
+    #
+    #         # For each pixel,
+    #         for row in data:
+    #             for value in row:
+    #                 # TODO run through network
+    #                 print(value)
 
-    # Traverse images in image_resized folder
-    rootDir = 'images/'
-    for dirName, subdirList, fileList in os.walk(rootDir):
-        for fileN in fileList:
-            # Get dog type from folder name
-            dirNameSplit = dirName.split('-')
-            dogName = dirNameSplit[1]
 
-            # Convert image into black and white. Rescale to 500x333 image
-            newDirName = dirName.replace('\\', '/')
-            img = Image.open(newDirName + '/' + fileN)
-            img = img.resize((image_width, image_height), PIL.Image.ANTIALIAS)
-            img = img.convert('L')
-            WIDTH, HEIGHT = img.size
-            data = list(img.getdata())
-            data = [data[offset:offset + WIDTH] for offset in range(0, WIDTH * HEIGHT, WIDTH)]
-
-            # For each pixel,
-            for row in data:
-                for value in row:
-                    # TODO run through network
-                    print(value)
-
+    brian_example()
 
     # TODO After training, test with user images (or run through images again and check accuracy)

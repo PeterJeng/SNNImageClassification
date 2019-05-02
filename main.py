a3600322a2bd965025293765d690bcd4e6e7ec9c
Francis <francisfull@gmail.com>
2019-05-02 17:16:14 -0400

    Implemented some sort of training. Not sure if it works

--- main.py
+++ main.py
@@ -3,73 +3,126 @@ from PIL import Image
 from brian2 import *
 from matplotlib import pyplot
 
-class LifNeuron:
-    def __init__(self, weights):
-        self.weights = weights
-        self.avg_rate = 0
-        self.num_occur = 0
 
-    def run(self, current):
-        resistance = 0.7
-        capacitor = 1
-        time_constant = resistance * capacitor
-        time_diff = 0.15
-        threshold_voltage = 1
-        end_time = 5
+if __name__ == '__main__':
+    # SETTING UP NETWORK
+    image_width = 100
+    image_height = 66
+    N = image_height * image_width
+    taum = 10 * ms
+    taupre = 20 * ms
+    taupost = taupre
+    Ee = 0 * mV
+    vt = -54 * mV
+    vr = -60 * mV
+    El = -74 * mV
+    taue = 5 * ms
+    F = 15 * Hz
+    gmax = .01
+    dApre = .01
+    dApost = -dApre * taupre / taupost * 1.05
+    dApost *= gmax
+    dApre *= gmax
 
-        # Increase steady input current after each test
-        current_constant = 0.15
-        cur_voltage = 0
-        cur_time = 0
-        num_spikes = 0
-        while cur_time < end_time:
-            cur_time += time_diff
+    eqs_neurons = '''
+    dv/dt = (ge * (Ee-vr) + El - v) / taum : volt
+    dge/dt = -ge / taue : 1
+    '''
 
-            cur_voltage_diff = (((-1 * cur_voltage) + (resistance * current)) / time_constant) * time_diff
-            cur_voltage = cur_voltage + cur_voltage_diff
+    # Input neurons - one for each pixel in the image
+    input_neurons = PoissonGroup(N, rates=(5 * Hz))
 
-            if cur_voltage >= threshold_voltage:
-                num_spikes += 1
-                current += current_constant
-                cur_voltage = 0
+    # Training neurons - correct dog image has higher rate of 15Hz, other neurons have 5Hz
+    training_neurons = PoissonGroup(120, rates=(5 * Hz))
 
-        cur_rate = num_spikes * (1000 / cur_time)
-        self.avg_rate = ((self.avg_rate * self.num_occur) + cur_rate) / (self.num_occur + 1)
-        self.num_occur += 1
-        return cur_rate
+    # Output neurons - one for each dog species
+    output_neurons = NeuronGroup(120, eqs_neurons, threshold='v>vt', reset='v = vr', method='exact')
 
-    def learn(self, weight_num, vi, vj, i_avg, j_avg):
-        weight_coeff = 0.000001
-        min_weight = 0.25
-        dif_weight = weight_coeff * (vi - i_avg) * (vj - j_avg)
-        '''
-        if weight_num == 1:
-            max_weight = 1.0 - self.w2 - self.w3
-            self.w1 += dif_weight
-            if self.w1 < min_weight:
-                self.w1 = min_weight
-            elif self.w1 > max_weight:
-                self.w1 = max_weight
-        elif weight_num == 2:
-            max_weight = 1.0 - self.w1 - self.w3
-            self.w2 += dif_weight
-            if self.w2 < min_weight:
-                self.w2 = min_weight
-            elif self.w2 > max_weight:
-                self.w2 = max_weight
-        elif weight_num == 3:
-            max_weight = 1.0 - self.w1 - self.w2
-            self.w3 += dif_weight
-            if self.w3 < min_weight:
-                self.w3 = min_weight
-            elif self.w3 > max_weight:
-                self.w3 = max_weight
-        '''
+    S = Synapses(input_neurons, output_neurons,
+                 '''w : 1
+                    dApre/dt = -Apre / taupre : 1 (event-driven)
+                    dApost/dt = -Apost / taupost : 1 (event-driven)''',
+                 on_pre='''ge += w
+                        Apre += dApre
+                        w = clip(w + Apost, 0, gmax)''',
+                 on_post='''Apost += dApost
+                         w = clip(w + Apre, 0, gmax)''',
+                 )
+    S.connect()
+    S2 = Synapses(training_neurons, output_neurons,
+                 '''w : 1
+                    dApre/dt = -Apre / taupre : 1 (event-driven)
+                    dApost/dt = -Apost / taupost : 1 (event-driven)''',
+                 on_pre='''ge += w
+                            Apre += dApre
+                            w = clip(w + Apost, 0, gmax)''',
+                 on_post='''Apost += dApost
+                             w = clip(w + Apre, 0, gmax)''',
+                 )
+    # Only connect training neuron to corresponding output neuron
+    for x in range(120):
+        S2.connect(i=x, j=x)
+
+    S.w = 'rand() * gmax'
+    S2.w = 'rand() * gmax'
 
 
-class NeuralNetwork:
-    def __init__(self):
-        self.layer1 = []
+    # TRAINING
+    # Traverse images in image_resized folder
+    root_dir = 'images/'
+    # Create list where dogList[i] = dog name for output neuron i
+    dog_list = []
+    for dir_name, subdir_list, file_list in os.walk(root_dir):
+        dir_name_split = dir_name.split('-')
+        if len(dir_name_split) > 1:
+            dog_name = dir_name_split[1]
+            dog_list.append(dog_name)
+            dog_num = len(dog_list) - 1
+        for file_name in file_list:
+            img = Image.open(dir_name + "/" + file_name)
+            img = img.resize((image_width, image_height), Image.ANTIALIAS)
+            img = img.convert('L')
+            data = list(img.getdata())
+            # Get value of pixel, divide it by 10 for input value
+            for i in range(len(data)):
+                data[i] = (data[i] / 10) * Hz
+
+            # Training inputs. All inputs give 5Hz except for correct output neuron, which gives 15Hz
+            # dog_num corresponds to the correct output neuron
+            training_inputs = [5 * Hz] * 120
+            training_inputs[dog_num] = 15 * Hz
+
+            input_neurons.rates = data
+            training_neurons.rates = training_inputs
+
+            print("Training with image: " + file_name)
+
+            run(10 * ms)
+
+    # TESTING
+    # Test with training data
+    store('after_learning')
+
+    for dir_name, subdir_list, file_list in os.walk(root_dir):
+        dir_name_split = dir_name.split('-')
+        if len(dir_name_split) > 1:
+            dog_name = dir_name_split[1]
+            dog_list.append(dog_name)
+            dog_num = len(dogList) - 1
+        for file_name in file_list:
+            restore('after_learning')
+            img = Image.open(dir_name + "/" + file_name)
+            img = img.resize((image_width, image_height), Image.ANTIALIAS)
+            img = img.convert('L')
+            data = list(img.getdata())
+
+            input_neurons.rates = data
+
+            run(100 * ms)
+
+            # TODO - Find output neuron with most spikes(?) to determine which output neuron corresponds with image
+            # If dog_num == most spiked output_neuron, then correct dog. Else wrong dog
+
 
 def brian_example():
     '''
@@ -96,118 +149,3 @@ def brian_example():
     show()
 
     '''
-
-def fj_example():
-    # Training
-    image_width = 100
-    image_height = 66
-
-    # CURRENTLY only using 1 test image. Should use traversal in main to train with all images
-    img = Image.open('test_img.jpg')
-    img = img.resize((image_width, image_height), Image.ANTIALIAS)
-    img = img.convert('L')
-    data = list(img.getdata())
-    for i in range(len(data)):
-        data[i] = (data[i] / 10) * Hz
-
-    N = image_height * image_width
-    taum = 10 * ms
-    taupre = 20 * ms
-    taupost = taupre
-    Ee = 0 * mV
-    vt = -54 * mV
-    vr = -60 * mV
-    El = -74 * mV
-    taue = 5 * ms
-    F = 15 * Hz
-    gmax = .01
-    dApre = .01
-    dApost = -dApre * taupre / taupost * 1.05
-    dApost *= gmax
-    dApre *= gmax
-
-    eqs_neurons = '''
-    dv/dt = (ge * (Ee-vr) + El - v) / taum : volt
-    dge/dt = -ge / taue : 1
-    '''
-
-    # Input neurons - one for each pixel in the image
-    input = PoissonGroup(N, rates=data)
-    # Output neurons - one for each dog species
-
-    # TODO - Get output neurons to spike the correct neuron cuz I dunno how (maybe add training neurons?)
-
-    neurons = NeuronGroup(120, eqs_neurons, rates=output_rates, threshold='v>vt', reset='v = vr',
-                          method='exact')
-    S = Synapses(input, neurons,
-                 '''w : 1
-                    dApre/dt = -Apre / taupre : 1 (event-driven)
-                    dApost/dt = -Apost / taupost : 1 (event-driven)''',
-                 on_pre='''ge += w
-                        Apre += dApre
-                        w = clip(w + Apost, 0, gmax)''',
-                 on_post='''Apost += dApost
-                         w = clip(w + Apre, 0, gmax)''',
-                 )
-    S.connect()
-    S.w = 'rand() * gmax'
-    mon = StateMonitor(S, 'w', record=[0, 1])
-    s_mon = SpikeMonitor(input)
-
-    run(100 * ms, report='text')
-
-
-    # Testing
-    store('after_learning')
-
-    keep_testing = true
-    while keep_testing:
-        restore('after_learning')
-
-
-
-if __name__ == '__main__':
-    '''
-    image_width = 500
-    image_height = 333
-    # Set up neural network
-    network = NeuralNetwork()
-    for row in range(0, image_height):
-        network.layer1.append([])
-        for col in range(0, image_width):
-            network.layer1[row].append(LifNeuron(0))
-
-    # Traverse images in image_resized folder
-    rootDir = 'images/'
-    # Create list where dogList[i] = dog name for output neuron i
-    dogList = []
-    for dirName, subdirList, fileList in os.walk(rootDir):
-        dirNameSplit = dirName.split('-')
-        if len(dirNameSplit) > 1:
-            dogName = dirNameSplit[1]
-            dogList.append(dogName)
-        for fileN in fileList:
-            # Get dog type from folder name
-            dirNameSplit = dirName.split('-')
-            dogName = dirNameSplit[1]
-            
-            # Convert image into black and white. Rescale to 500x333 image
-            newDirName = dirName.replace('\\', '/')
-            img = Image.open(newDirName + '/' + fileN)
-            img = img.resize((image_width, image_height), Image.ANTIALIAS)
-            img = img.convert('L')
-            WIDTH, HEIGHT = img.size
-            data = list(img.getdata())
-            data = [data[offset:offset + WIDTH] for offset in range(0, WIDTH * HEIGHT, WIDTH)]
-
-            # For each pixel,
-            for row in data:
-                for value in row:
-                    # TODO run through network
-                    print(value)
-                    
-    '''
-
-    fj_example()
-
-    # TODO After training, test with user images (or run through images again and check accuracy)

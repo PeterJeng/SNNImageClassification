40bd34840f72a01c7ad651d6b5ad73b1495c0344
JosephJaeschke <joeyj987@gmail.com>
2019-05-06 21:52:01 -0400

    find highest spiking neuron

--- main.py
+++ main.py
@@ -1,11 +1,39 @@
 import os
+import numpy as np
 from PIL import Image
 from brian2 import *
 from matplotlib import pyplot
 
 
+def brian_example():
+    eqs = '''
+
+    dv/dt  = (ge+gi-(v+49*mV))/(20*ms) : volt
+    dge/dt = -ge/(5*ms)                : volt
+    dgi/dt = -gi/(10*ms)               : volt
+    '''
+
+    P = NeuronGroup(6600, eqs, threshold='v>-50*mV', reset='v=-60*mV')
+    P.v = -60 * mV
+    Pe = P[:3200]
+    Pi = P[3200:]
+    Ce = Synapses(Pe, P, on_pre='ge+=1.62*mV')
+    Ce.connect(p=0.02)
+    Ci = Synapses(Pi, P, on_pre='gi-=9*mV')
+    Ci.connect(p=0.02)
+    M = SpikeMonitor(P)
+    run(1 * second)
+    maxNeuron=np.argmax(M.count)
+    maxCount=np.max(M.count)
+    print maxNeuron, maxCount
+    plot(M.t / ms, M.i, '.')
+    show()
+ 
+
 if __name__ == '__main__':
     # SETTING UP NETWORK
+    brian_example()
+    exit()
     image_width = 100
     image_height = 66
     N = image_height * image_width
@@ -153,30 +181,3 @@ if __name__ == '__main__':
 
     print("Using testing data, the network correctly identified: " + str(tests_correct / tests_total) +
           "% of the images")
-
-
-def brian_example():
-    '''
-    eqs = '''
-
-    '''
-    dv/dt  = (ge+gi-(v+49*mV))/(20*ms) : volt
-    dge/dt = -ge/(5*ms)                : volt
-    dgi/dt = -gi/(10*ms)               : volt
-    '''
-
-    '''
-    P = NeuronGroup(6600, eqs, threshold='v>-50*mV', reset='v=-60*mV')
-    P.v = -60 * mV
-    Pe = P[:3200]
-    Pi = P[3200:]
-    Ce = Synapses(Pe, P, on_pre='ge+=1.62*mV')
-    Ce.connect(p=0.02)
-    Ci = Synapses(Pi, P, on_pre='gi-=9*mV')
-    Ci.connect(p=0.02)
-    M = SpikeMonitor(P)
-    run(1 * second)
-    plot(M.t / ms, M.i, '.')
-    show()
-
-    '''

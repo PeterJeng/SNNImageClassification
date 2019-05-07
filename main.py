a9b59e23d276d645a0bcd36beab6e44290748492
JosephJaeschke <joeyj987@gmail.com>
2019-05-07 03:14:07 -0400

    save weights

--- main.py
+++ main.py
@@ -6,6 +6,21 @@ from matplotlib import pyplot
 
 
 def brian_example():
+    taum = 10 * ms
+    taupre = 20 * ms
+    taupost = taupre
+    Ee = 0 * mV
+    vt = -54 * mV
+    vr = -60 * mV
+    El = -74 * mV
+    taue = 5 * ms
+    F = 15 * Hz
+    gmax = .01
+    dApre = .01
+    dApost = -dApre * taupre / taupost * 1.05
+    dApost *= gmax
+    dApre *= gmax
+
     eqs = '''
 
     dv/dt  = (ge+gi-(v+49*mV))/(20*ms) : volt
@@ -13,21 +28,33 @@ def brian_example():
     dgi/dt = -gi/(10*ms)               : volt
     '''
 
-    P = NeuronGroup(6600, eqs, threshold='v>-50*mV', reset='v=-60*mV')
+    P = NeuronGroup(5, eqs, threshold='v>-50*mV', reset='v=-60*mV')
+    P2 = NeuronGroup(5, eqs, threshold='v>-50*mV', reset='v=-60*mV')
     P.v = -60 * mV
-    Pe = P[:3200]
-    Pi = P[3200:]
-    Ce = Synapses(Pe, P, on_pre='ge+=1.62*mV')
-    Ce.connect(p=0.02)
-    Ci = Synapses(Pi, P, on_pre='gi-=9*mV')
-    Ci.connect(p=0.02)
-    M = SpikeMonitor(P)
-    run(1 * second)
+    P2.v=-60*mV
+    Ce = Synapses(P, P2,
+                 '''w : 1
+                    dApre/dt = -Apre / taupre : 1 (event-driven)
+                    dApost/dt = -Apost / taupost : 1 (event-driven)''',
+                 on_pre='ge+=1.62*mV'
+                 )
+
+    Ce.connect()
+    #Ci = Synapses(Pi, P, on_pre='gi-=9*mV')
+    #Ci.connect(p=0.02)
+    Ce.w='rand()*0.01'
+    M = SpikeMonitor(P2)
+    SM=StateMonitor(Ce,'w',record=True)
+    run(20 * ms)
     maxNeuron=np.argmax(M.count)
     maxCount=np.max(M.count)
-    print maxNeuron, maxCount
-    plot(M.t / ms, M.i, '.')
-    show()
+    for i in range(5):
+        print SM[Ce[i,:]].w
+
+    for i in range(5):
+        print "Neuron ",i
+        for j in range(5):
+            print SM[Ce[i,j]].w[0][len(SM[Ce[i,j]].w)]
  
 
 if __name__ == '__main__':
@@ -95,8 +122,12 @@ if __name__ == '__main__':
     S.w = 'rand() * gmax'
     S2.w = 'rand() * gmax'
 
+    #Monitors for synapse weights
+    monitorS=StateMonitor(S,'w',record=True)
+    monitorS2=StateMonitor(S2,'w',record=True)
 
     # TRAINING
+    print "training"
     # Traverse images in image_resized folder
     root_dir = 'images/'
     counter = 0
@@ -132,8 +163,26 @@ if __name__ == '__main__':
                 run(50 * ms)
 
                 print(output_neurons.spikes)
+	#Save trained synaptic weights
+	f=open('S.csv','w+')
+    for i in range(N):
+        for j in range(4000):
+            f.write(monitorS[S[i,j]].w[0][len(monitorS[S[i,j]].w)])
+            f.write(',')
+        f.write('\n')
+    f.close()
+    f2=open('S2.csv','w+')
+    for i in range(total_dog_species):
+        for j in range(4000):
+            f2.write(monitorS2[S2[i,j]].w[0][len(monitorS2[S2[i,j]].w)])
+            f2.write(',')
+        f2.write('\n')
+    f2.close()
+
+
 
     # TESTING
+    print "testing"
     # Test with training data
     store('after_learning')
 
@@ -161,15 +210,21 @@ if __name__ == '__main__':
                 tests_total += 1.0
 
                 input_neurons.rates = data
-
-                run(20 * ms)
+                outMonitor=SpikeMonitor(output_neurons)
+                run(50 * ms)
 
                 # Of the output_neurons spiked, check if correct dog species spikes
-                spikes = output_neurons.spikes
+                #spikes = output_neurons.spikes
 
                 print("Testing with image: " + file_name + " : dog_num = " + str(dog_num))
-                print(spikes)
+                #Find output neuron with highest spike frequency
+                maxNeuron=np.argmax(outMonitor.count)
+                #maxCount=np.max(outMonitor.count)
+                if maxNeuron==dog_num:
+                    tests_correct+=1.0
 
+                #print(spikes)
+                '''
                 i = 0
                 while i < len(spikes) and spikes[i] <= dog_num:
                     # Correct output neuron spiked
@@ -178,6 +233,6 @@ if __name__ == '__main__':
                         i = len(spikes)
                     else:
                         i += 1
-
+               '''
     print("Using testing data, the network correctly identified: " + str(tests_correct / tests_total) +
           "% of the images")
